06c1e2c8a0aa9fc70afca6224f6dd24424eb239f
CBU <he01314906@gmail.com>
2024-08-22 16:26:12 +0800

    first commit

--- src/hw2/hw2.java
+++ src/hw2/hw2.java
@@ -95,5 +95,37 @@ public class hw2 {
 //			
 //			System.out.println(s[i]);
 //		}
+		
+		// 99乘法表
+		for (int i = 1; i <= 9; i++) {
+			int j = 1;
+			while (j <= 9) {
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+				j++;
+			}
+			System.out.println("");
+			
+		}
+
+//		for (int i = 1; i <= 9; i++) {
+//			int j = 1;
+//			 do{
+//				System.out.print(i + "*" + j + "=" + i * j + "\t");
+//				j++;
+//			}while (j <= 9);
+//			System.out.println("");
+//		}
+//		int i = 1;
+//
+//		while (i <= 9) {
+//			int j = 1;
+//			do {
+//				System.out.print(i + "*" + j + "=" + i * j + "\t");
+//				j++;
+//			} while (j <= 9);
+//			i++;
+//			System.out.println("");
+//		}
+		
 	}
 }
