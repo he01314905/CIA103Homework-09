e8a2b399066ef3f807ec6cc82097a616b066121b
CBU <he01314906@gmail.com>
2024-09-05 11:53:13 +0800

    hw6

--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -6,32 +6,30 @@ import java.util.Scanner;
 public class CalTest {
 	public static void main(String[] args) throws InputMismatchException {
 		Calculator pow = new Calculator();
-		Scanner sc = new Scanner(System.in);
 		boolean c = true;
-		System.out.println("請輸入x的值:");
-		System.out.println("請輸入y的值:");
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		if(sc.hasNextInt()) {
-			 x = sc.nextInt();
-		}else {
-			System.out.println("輸入格式錯誤");
-		}
-	
 		while (c) {
 			try {
+				Scanner sc = new Scanner(System.in);
+				System.out.println("請輸入x的值:");
+
+				if (sc.hasNextInt() == false) {
+					throw new InputMismatchException("輸入格式錯誤");
+				}
+				int x = sc.nextInt();
+
+				System.out.println("請輸入y的值:");
+				if (sc.hasNextInt() == false) {
+					throw new InputMismatchException("輸入格式錯誤");
+				}
+				int y = sc.nextInt();
+
 				pow.powerXY(x, y);
 				c = false;
+
 			} catch (CalExecption e) {
 				System.out.println(e.getMessage());
-				System.out.println("請輸入x的值:");
-				x = sc.nextInt();
-				System.out.println("請輸入y的值:");
-				y = sc.nextInt();
-			} catch (InputMismatchException e) {
-				e.printStackTrace();
-			} finally {
-				sc.close();
+			} catch (InputMismatchException ex) {
+				System.out.println(ex.getMessage());
 			}
 		}
 	}
