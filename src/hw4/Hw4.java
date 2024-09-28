dd4cb8a4d9479a8f3e4feb5a304e277d8702092d
CBU <he01314906@gmail.com>
2024-09-28 23:22:04 +0800

    update

--- src/hw4/Hw4.java
+++ src/hw4/Hw4.java
@@ -119,18 +119,21 @@ public class Hw4 {
 		int score[][] = { { 10, 37, 100, 77, 98, 90 }, { 35, 75, 70, 95, 70, 80 }, { 40, 77, 79, 70, 89, 100 },
 				{ 100, 89, 90, 89, 90, 75 }, { 90, 64, 75, 60, 75, 50 }, { 85, 75, 70, 75, 90, 20 },
 				{ 75, 70, 79, 85, 89, 99 }, { 70, 95, 90, 89, 90, 75 } };
-		int exam;
-		int sctime;
-		for (int i = 0; i < score.length; i++) {
-			exam = 0;
-			sctime = 0;
-			for (int j = 0; j < score[i].length; j++) {
+		int sctop[] = new int[8];
+		
+		for (int j = 0; j < 6; j++) {
+			int exam = 0;
+			int sctime = 0;
+			for (int i = 0; i < score.length; i++) {
 				if (score[i][j] > exam) {
 					exam = score[i][j];
-					sctime = j + 1;
+					sctime = i;
 				}
 			}
-			System.out.println((i + 1) + "號" + "第" + sctime + "次成績最高:" + exam);
+			sctop[sctime] += 1;
+		}
+		for (int k = 0; k < sctop.length; k++) {
+			System.out.println("第 " + (k + 1) + " 號學生考 " + sctop[k] + " 次最高分");
 		}
 	}
 }
